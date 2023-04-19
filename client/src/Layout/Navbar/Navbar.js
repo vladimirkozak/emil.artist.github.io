19d05a57400173b2df6ac06f06fd396b5ee0d5d7
vladimir kozak <v.kozak@sotbit.ru>
2023-04-19 11:04:01 +0300

    feat: add NavLink

--- client/src/Layout/Navbar/Navbar.js
+++ client/src/Layout/Navbar/Navbar.js
@@ -1,6 +1,35 @@
+import { NavLink } from "react-router-dom";
+import './Navbar.scss';
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className="header">
+      <div>
+        Logo
+      </div>
+      <nav className="navigation">
+        <ul className="navigation__list">
+          <li className="navigation__item">
+            <NavLink
+              to='/'
+              activeClassName='active'
+              className="navigation__link"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="navigation__item">
+            <NavLink
+              to='/catalog'
+              activeClassName='active'
+              className="navigation__link"
+            >
+              Catalog
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
