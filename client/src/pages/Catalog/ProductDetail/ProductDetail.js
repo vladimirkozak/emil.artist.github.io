8a35f9f26ea4da36874d2e9d67bdcea8aa173371
vladimir kozak <v.kozak@sotbit.ru>
2024-05-03 17:25:38 +0300

    minor

--- client/src/pages/Catalog/ProductDetail/ProductDetail.js
+++ client/src/pages/Catalog/ProductDetail/ProductDetail.js
@@ -11,6 +11,7 @@ import Select, { SelectChangeEvent } from "@mui/material/Select";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import { DUMMY_CATALOG } from "../testData/data/data";
+import Container from "@components/containers/Container/Container";
 
 const ProductDetail = () => {
   const params = useParams();
@@ -32,83 +33,85 @@ const ProductDetail = () => {
   const onClickHandler = () => {};
 
   return (
-    <section className="product">
-      <div className="product-container">
-        <div className="left">
-          <div className="images">
-            <img
-              src={DUMMY_CATALOG[0].img}
-              onClick={() => setSelectedImg(0)}
-              alt="detail_img"
-            />
-            <img
-              src={DUMMY_CATALOG[1].img}
-              onClick={() => setSelectedImg(1)}
-              alt="detail_img"
-            />
+    <Container>
+      <section className="product">
+        <div className="product-container">
+          <div className="left">
+            <div className="images">
+              <img
+                src={DUMMY_CATALOG[0].img}
+                onClick={() => setSelectedImg(0)}
+                alt="detail_img"
+              />
+              <img
+                src={DUMMY_CATALOG[1].img}
+                onClick={() => setSelectedImg(1)}
+                alt="detail_img"
+              />
+            </div>
+            <div className="mainImg">
+              <img src={DUMMY_CATALOG[selectedImg].img} alt="main_detail_img" />
+            </div>
           </div>
-          <div className="mainImg">
-            <img src={DUMMY_CATALOG[selectedImg].img} alt="main_detail_img" />
-          </div>
-        </div>
-        <div className="right">
-          <h5 className="title">{DUMMY_CATALOG[0].title}</h5>
-          <span>{DUMMY_CATALOG[0].price}</span>
-          <div className="props">
-            <div className="select">
-              <Box sx={{ minWidth: 120 }}>
-                <FormControl fullWidth size="small">
-                  <InputLabel id="demo-simple-select-label">
-                    {t("catalog.size")}
-                  </InputLabel>
-                  <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={size}
-                    label="Size"
-                    onChange={handleChange}
+          <div className="right">
+            <h5 className="title">{DUMMY_CATALOG[0].title}</h5>
+            <span>{DUMMY_CATALOG[0].price}</span>
+            <div className="props">
+              <div className="select">
+                <Box sx={{ minWidth: 120 }}>
+                  <FormControl fullWidth size="small">
+                    <InputLabel id="demo-simple-select-label">
+                      {t("catalog.size")}
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={size}
+                      label="Size"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={"S"}>S</MenuItem>
+                      <MenuItem value={"M"}>M</MenuItem>
+                      <MenuItem value={"L"}>L</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+              </div>
+              <div className="quantity">
+                <Stack direction="row" spacing={2}>
+                  <Button
+                    className="quantity__btn"
+                    variant="contained"
+                    onClick={() =>
+                      setQuantity((prev) => (prev === 1 ? 1 : prev - 1))
+                    }
                   >
-                    <MenuItem value={"S"}>S</MenuItem>
-                    <MenuItem value={"M"}>M</MenuItem>
-                    <MenuItem value={"L"}>L</MenuItem>
-                  </Select>
-                </FormControl>
-              </Box>
+                    -
+                  </Button>
+                  <span className="quantity__count">{quantity}</span>
+                  <Button
+                    className="quantity__btn"
+                    variant="contained"
+                    onClick={() => setQuantity((prev) => prev + 1)}
+                  >
+                    +
+                  </Button>
+                </Stack>
+              </div>
+              <div className="add-to-cart">
+                <Btn title={t("cart.addToCart")} onClick={onClickHandler} background={'#ffb301'} />
+              </div>
             </div>
-            <div className="quantity">
-              <Stack direction="row" spacing={2}>
-                <Button
-                  className="quantity__btn"
-                  variant="contained"
-                  onClick={() =>
-                    setQuantity((prev) => (prev === 1 ? 1 : prev - 1))
-                  }
-                >
-                  -
-                </Button>
-                <span className="quantity__count">{quantity}</span>
-                <Button
-                  className="quantity__btn"
-                  variant="contained"
-                  onClick={() => setQuantity((prev) => prev + 1)}
-                >
-                  +
-                </Button>
-              </Stack>
+            <div className="desc">
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and Lorem Ipsum
+                is simply dummy text of the printing and
+              </p>
             </div>
-            <div className="add-to-cart">
-              <Btn title={t("cart.addToCart")} onClick={onClickHandler} />
-            </div>
-          </div>
-          <div className="desc">
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and Lorem Ipsum
-              is simply dummy text of the printing and
-            </p>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Container>
   );
 };
 
